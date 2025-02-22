5ab7a2bea262282590a8c58a4868d97b2140a71e
confusedalex <alex@confusedalex.dev>
2025-02-22 08:46:58 +0100

    feat: add more fzf-lua keybinds

--- config/ui/fzf-lua.nix
+++ config/ui/fzf-lua.nix
@@ -11,6 +11,18 @@
         action = "live_grep";
         options.desc = "Find text";
       };
+      "<leader>fd" = {
+        action = "diagnostics_document";
+        options.desc = "Find buffer diagnostics";
+      };
+      "<leader>fD" = {
+        action = "diagnostics_workspace";
+        options.desc = "Find workspace diagnostics";
+      };
+      "<leader>fs" = {
+        action = "lsp_document_symbols";
+        options.desc = "Find lsp document symbols";
+      };
     };
   };
 }
